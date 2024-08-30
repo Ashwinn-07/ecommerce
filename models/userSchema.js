3ded3def92d2ef38161e63d625cfb33f74619556
Ashwinn-07 <ashwinkrishna785@gmail.com>
2024-08-30 15:49:37 +0530

    models done

---
+++ models/userSchema.js
@@ -0,0 +1,85 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  cart: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cart",
+    },
+  ],
+  wallet: {
+    type: Number,
+    default: 0,
+  },
+  wishlist: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Wishlist",
+    },
+  ],
+  orderHistory: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+  referralCode: {
+    type: String,
+  },
+  redeemed: {
+    type: Boolean,
+  },
+  redeemedUsers: [
+    {
+      type: Mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  searchHistory: [
+    {
+      category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+      },
+      brand: {
+        type: String,
+      },
+      searchOn: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
