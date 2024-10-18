3cc19217bb336c967ebe9dda97cea72bdeaad826
Ashwinn-07 <ashwinkrishna785@gmail.com>
2024-10-18 07:07:26 +0530

    db changed

--- config/db.js
+++ config/db.js
@@ -3,11 +3,14 @@ const env = require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "ecommerce",
+    });
     console.log("DB connected");
   } catch (error) {
-    console.log("error connecting", error.message);
+    console.log("Error connecting to MongoDB:", error.message);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
